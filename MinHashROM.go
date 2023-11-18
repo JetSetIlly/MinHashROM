efbe3d82faa1ee7b2ab884e0bf158939df9444b4
JetSetIlly <stephen.t.illingworth@gmail.com>
2023-11-18 16:53:51 +0000

    added CREATE and MATCH mode
    program now requires the database to be created before matching

--- MinHashROM.go
+++ MinHashROM.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
+	"encoding/binary"
+	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/dgryski/go-farm"
 	"github.com/dgryski/go-spooky"
@@ -14,74 +19,346 @@ import (
 )
 
 const (
-	romSize   = 4096
-	chunkSize = 64
+	supportedVersion = 1
+	defaultChunkSize = 64
+	readBlockSize    = 256
+	hashSize         = 8
 )
 
+type minHashRom struct {
+	programName string
+	mode        string
+	args        []string
+}
+
 func main() {
-	log.SetFlags(0)
+	var mhr minHashRom
+	mhr.programName = filepath.Base(os.Args[0])
+
 	var err error
 
-	if len(os.Args) != 3 {
-		programName := filepath.Base(os.Args[0])
-		log.Fatalf("usage: %s <rom directory> <rom to compare>\n", programName)
+	// find mode
+	if len(os.Args) > 1 {
+		mhr.mode = strings.ToUpper(os.Args[1])
 	}
 
-	romDir, err := filepath.EvalSymlinks(os.Args[1])
+	switch mhr.mode {
+	case "CREATE":
+		mhr.args = os.Args[2:]
+		err = mhr.create()
+	case "MATCH":
+		mhr.args = os.Args[2:]
+		err = mhr.match()
+	default:
+		mhr.mode = "MATCH"
+		mhr.args = os.Args[1:]
+		err = mhr.match()
+	}
+
 	if err != nil {
-		log.Fatalf("rom directory: %v", err)
-		return
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func (mhr minHashRom) create() error {
+	var chunkSize int
+	var dbFile string
+
+	flgs := flag.NewFlagSet(mhr.mode, flag.ExitOnError)
+	flgs.IntVar(&chunkSize, "c", defaultChunkSize, "chunk size")
+	flgs.StringVar(&dbFile, "db", "minhash.db", "name of minhash database file")
+
+	flgs.Usage = func() {
+		fmt.Printf("usage: %s %s [ROM directory]\n", mhr.programName, mhr.mode)
+		flgs.PrintDefaults()
 	}
 
-	cmpFile, err := filepath.EvalSymlinks(os.Args[2])
+	flgs.Parse(mhr.args)
+
+	// parameter check
+	if chunkSize < 8 {
+		return fmt.Errorf("chunk size of less than 8 is pointless")
+	}
+	if chunkSize > 2048 {
+		return fmt.Errorf("chunk size of more than 2048 is pointless")
+	}
+	if 4096%chunkSize != 0 {
+		return fmt.Errorf("chunk size must divide equally into 4096")
+	}
+
+	args := flgs.Args()
+	if len(args) != 1 {
+		flgs.Usage()
+		return nil
+	}
+	roms := args[0]
+
+	db, err := os.Create(dbFile)
 	if err != nil {
-		log.Fatalf("rom to compare: %v", err)
-		return
+		return fmt.Errorf("error creating database: %w", err)
 	}
+	defer db.Close()
 
-	f, err := os.ReadFile(cmpFile)
-	if err != nil {
-		log.Fatalf("error reading rom: %s: %v", cmpFile, err)
-	}
-	if len(f) != romSize {
-		log.Fatalf("only ROMs of %d bytes are supported for now: %s: %v", romSize, cmpFile, err)
-	}
+	// write header
+	db.Write([]byte{
+		byte(supportedVersion >> 8),
+		byte(supportedVersion),
+	})
+	db.Write([]byte{
+		byte(chunkSize >> 8),
+		byte(chunkSize),
+	})
 
-	cmpmw := minhash.New(spooky.Hash64, farm.Hash64, romSize/chunkSize)
-	for i := 0; i < len(f); i += chunkSize {
-		cmpmw.Push(f[i : i+chunkSize-1])
-	}
+	// progress
+	fmt.Printf("creating db file %s\n", dbFile)
+	fmt.Printf("chunk size %d\n", chunkSize)
 
-	var roms []string
+	var entryCount int
 
-	err = filepath.Walk(romDir, func(romFile string, info fs.FileInfo, err error) error {
-		if !info.IsDir() && info.Size() == romSize {
-			roms = append(roms, romFile)
+	createFunc := func(path string, mw *minhash.MinHash) error {
+		// write path to dbfile making it's not too long
+		path = filepath.Base(path)
+		path = path[:min(readBlockSize-1, len(path))]
+		db.Write([]byte(path))
+		db.Write([]byte{0x00})
+
+		var buffer [8]byte
+
+		for _, sig := range mw.Signature() {
+			binary.LittleEndian.PutUint64(buffer[:], sig)
+			_, err := db.Write(buffer[:])
+			if err != nil {
+				return fmt.Errorf("error creating database: %w", err)
+			}
 		}
+
+		entryCount++
+		return nil
+	}
+
+	err = mhr.walkRomDirectory(roms, chunkSize, createFunc)
+	if err != nil {
+		return err
+	}
+
+	if entryCount == 1 {
+		fmt.Printf("%d entry written\n", entryCount)
+	} else {
+		fmt.Printf("%d entries written\n", entryCount)
+	}
+
+	return nil
+}
+
+func (mhr minHashRom) match() error {
+	var sensitivity float64
+	var dbFile string
+	var verbose bool
+
+	flgs := flag.NewFlagSet(mhr.mode, flag.ExitOnError)
+
+	flgs.Float64Var(&sensitivity, "s", 80.0, "match sensitivity")
+	flgs.StringVar(&dbFile, "db", "minhash.db", "name of minhash database file")
+	flgs.BoolVar(&verbose, "v", false, "verbose output")
+
+	flgs.Usage = func() {
+		fmt.Printf("usage: %s %s [comparison ROM]\n", mhr.programName, mhr.mode)
+		flgs.PrintDefaults()
+	}
+
+	flgs.Parse(mhr.args)
+
+	args := flgs.Args()
+	if len(args) != 1 {
+		flgs.Usage()
+		return nil
+	}
+
+	// load comparison rom
+	path, err := filepath.EvalSymlinks(args[0])
+	if err != nil {
+		return fmt.Errorf("ROM does not exist: %s", args[0])
+	}
+
+	f, err := loadROM(path)
+	if err != nil {
+		return err
+	}
+
+	// open database
+	db, err := os.Open(dbFile)
+	if err != nil {
+		return fmt.Errorf("cannot open database: %s", dbFile)
+	}
+	defer db.Close()
+
+	var buffer [readBlockSize]byte
+
+	// read database header
+	n, err := db.Read(buffer[:4])
+	if err != nil {
+		return fmt.Errorf("error reading database: %w", err)
+	}
+	if n != 4 {
+		return fmt.Errorf("invalid database file: no header found")
+	}
+
+	if supportedVersion != (int(buffer[0])<<8)|int(buffer[1]) {
+		return fmt.Errorf("invalid database file: unsupported version")
+	}
+
+	chunkSize := (int(buffer[2]) << 8) | int(buffer[3])
+	if chunkSize <= 0 || 4096%chunkSize != 0 {
+		return fmt.Errorf("invalid database file: invalid chunk size")
+	}
+
+	// number of signatures per rom
+	numSigs := 4096 / chunkSize
+
+	// progress
+	if verbose {
+		fmt.Printf("matching with db file %s\nwith chunk size %d\n", dbFile, chunkSize)
+		fmt.Println("------")
+	}
+
+	// prepare minhash for comparison rom
+	cmp := minhash.New(spooky.Hash64, farm.Hash64, 4096/chunkSize)
+	for i := 0; i < len(f); i += chunkSize {
+		cmp.Push(f[i : i+chunkSize-1])
+	}
+
+	var done bool
+	var matchCount int
+	var entryCount int
+
+	if verbose {
+		defer func() {
+			fmt.Println("------")
+			if matchCount == 1 {
+				fmt.Printf("%d entry matched\n", matchCount)
+			} else {
+				fmt.Printf("%d entries matched\n", matchCount)
+			}
+			if entryCount == 1 {
+				fmt.Printf("%d entry checked\n", entryCount)
+			} else {
+				fmt.Printf("%d entries checked\n", entryCount)
+			}
+		}()
+	}
+
+	for !done {
+		// read rom name from database
+		n, err = db.Read(buffer[:])
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return fmt.Errorf("error reading database: %w", err)
+		}
+
+		// the amount of data read was less than the block size, this means this
+		// will be the last iteration
+		done = done || n < readBlockSize
+
+		n = bytes.IndexByte(buffer[:n], 0x00)
+		if n == -1 {
+			return fmt.Errorf("invalid database file: unexpected end of file")
+		}
+		rom := string(buffer[:n])
+
+		// position file at start of hashes for the file
+		_, err = db.Seek(-int64(len(buffer)-n-1), io.SeekCurrent)
+		if err != nil {
+			return fmt.Errorf("error reading database: %w", err)
+		}
+
+		// read all signatures from rom
+		var sigs []uint64
+
+		for i := 0; i < numSigs; i++ {
+			n, err = db.Read(buffer[:hashSize])
+			if err != nil {
+				return fmt.Errorf("error reading database: %w", err)
+			}
+			if n != hashSize {
+				return fmt.Errorf("invalid database file: unexpected end of file")
+			}
+
+			sig := binary.LittleEndian.Uint64(buffer[:])
+			sigs = append(sigs, sig)
+		}
+
+		mw := minhash.New(spooky.Hash64, farm.Hash64, 4096/chunkSize)
+		mw.SetSignature(sigs)
+
+		s := minhash.Similarity(mw, cmp) * 100
+		if s >= sensitivity {
+			fmt.Printf("%6.02f%%   %s \n", s, rom)
+			matchCount++
+		}
+
+		entryCount++
+	}
+
+	return nil
+}
+
+func (mhr minHashRom) walkRomDirectory(roms string, chunkSize int, hook func(string, *minhash.MinHash) error) error {
+	err := filepath.Walk(roms, func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		// ignore files that are very big
+		if info.Size() >= 65536 {
+			return nil
+		}
+
+		f, err := loadROM(path)
+		if err != nil {
+			if errors.Is(err, unsupportedFileSize) {
+				return nil
+			}
+			return err
+		}
+
+		mw := minhash.New(spooky.Hash64, farm.Hash64, 4096/chunkSize)
+		for i := 0; i < len(f); i += chunkSize {
+			mw.Push(f[i : i+chunkSize-1])
+		}
+
+		err = hook(path, mw)
+		if err != nil {
+			return err
+		}
+
 		return nil
 	})
 	if err != nil {
-		log.Fatalf("error reading ROMs: %v", err)
-		return
+		return fmt.Errorf("error reading ROMs: %w", err)
 	}
 
-	var highest float64
+	return nil
+}
 
-	for _, romFile := range roms {
-		f, err := os.ReadFile(romFile)
-		if err != nil {
-			log.Fatalf("error reading rom: %s: %v", cmpFile, err)
-		}
-		rommw := minhash.New(spooky.Hash64, farm.Hash64, romSize/chunkSize)
-		for i := 0; i < len(f); i += chunkSize {
-			rommw.Push(f[i : i+chunkSize-1])
-		}
+var unsupportedFileSize = errors.New("unsupported file size")
 
-		s := minhash.Similarity(rommw, cmpmw) * 100
-		if s > 0 && s >= highest {
-			fmt.Printf("%6.02f%%   %s \n", s, filepath.Base(romFile))
-			highest = s
-		}
+func loadROM(path string) ([]byte, error) {
+	f, err := os.ReadFile(path)
+	if err != nil {
+		return []byte{}, fmt.Errorf("error opening %s", path)
 	}
 
+	if len(f) == 2048 {
+		// 2k files need to be doubled up to 4096 bytes
+		return append(f, f...), nil
+	} else if len(f) >= 4096 {
+		// cap files to 4096 bytes
+		return f[:4096], nil
+	} else {
+		// files shorter than 4096 but not 2048 are not supported
+		return []byte{}, fmt.Errorf("%w: file size: %d bytes", unsupportedFileSize, len(f))
+	}
 }
